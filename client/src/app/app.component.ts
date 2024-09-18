c779f53164bfe9eb8bba5b6deccd08aa188e563d
LoseeT <loseet@gmatc.matc.edu>
2024-09-17 20:05:49 -0500

    Updated app.components.ts

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,18 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'client';
+  weatherForecasts: any;
+
+  constructor(private http: HttpClient){
+
+  }
+  ngOnInit(): void {
+    this.http.get('http://localhost:5278/weatherforecast').subscribe({
+      next: (response) => this.weatherForecasts = response,
+      error: (e) => console.error(e),
+      complete: () => console.log('complete')
+    })
+  }
 }
